c80ecbe09e9bad4cf7add7c3dcd859fe4125180b
Razeb Enyi <enyirazeb@gmail.com>
2022-12-15 11:52:31 +0100

    removed modules

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,9 +2,6 @@
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss", "@nuxtjs/robots"],
   css: ["@/assets/globals.css"],
-//   build: {
-//     transpile: ['@heroicons/vue']
-// },
   sitemap: {
     hostname: "https://gynescope.com.ng",
     trailingSlash: true
