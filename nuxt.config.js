be2a04ac783095a48aabf766974c34c3c0428eb2
Razeb Enyi <enyirazeb@gmail.com>
2022-12-15 12:02:29 +0100

    removed modules

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,18 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/robots"],
+  modules: ["@nuxtjs/tailwindcss"],
   css: ["@/assets/globals.css"],
-  sitemap: {
-    hostname: "https://gynescope.com.ng",
-    trailingSlash: true
-  },
+  // sitemap: {
+  //   hostname: "https://gynescope.com.ng",
+  //   trailingSlash: true
+  // },
 
-  // tell web crawlers where to crawl
-  robots: {
-    UserAgent: "*",
+  // // tell web crawlers where to crawl
+  // robots: {
+  //   UserAgent: "*",
 
-    Disallow: () => ["/thanks/", "/200.html", "/README.md"],
+  //   Disallow: () => ["/thanks/", "/200.html", "/README.md"],
 
-    Sitemap: "https://gynescope.com.ng/sitemap.xml"
-  }
+  //   Sitemap: "https://gynescope.com.ng/sitemap.xml"
+  // }
 });
