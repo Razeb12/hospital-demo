247289f7d2cd5e1d7ad6283444553de14d967ad4
Razeb Enyi <enyirazeb@gmail.com>
2022-12-15 13:36:28 +0100

    updated js to ts

---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/robots"],
+  css: ["@/assets/globals.css"],
+  // sitemap: {
+  //   hostname: "https://gynescope.com.ng",
+  //   trailingSlash: true
+  // },
+  // target: "static",
+  // tell web crawlers where to crawl
+  // robots: {
+  //   UserAgent: "*",
+
+  //   Disallow: () => ["/thanks/", "/200.html", "/README.md"],
+
+  //   Sitemap: "https://gynescope.com.ng/sitemap.xml"
+  // }
+});
